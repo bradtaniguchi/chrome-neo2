512c0c0e0f5ea693b7a71732aacb6b6698e5a747
brad taniguchi <bradtaniguchi@gmail.com>
2021-01-24 21:39:50 -0800

    Remove spec files

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'chrome-neo2-root',
+  selector: 'neo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
